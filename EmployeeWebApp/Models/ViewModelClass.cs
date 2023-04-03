0eeeecb50e00773fa1e5028e55b1091548fe4747
Tarun Kaushik <tarun.kaushik@enercross.com>
2023-04-03 14:41:09 +0530

    Revert "Revert "Add project files.""
    This reverts commit 1a84db86b034997b689c702d1488b5413b912c6d.

---
+++ EmployeeWebApp/Models/ViewModelClass.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using System.Xml.Linq;
+
+namespace EmployeeWebApp.Models
+{
+    public class ViewModelClass
+    {
+        [DataMember(Name = "Employee_Id")]
+        public int? Employee_Id { get; set; }
+        [DataMember(Name = "Name")]
+        public string? Name { get; set; }
+        [DataMember(Name = "Age")]
+        [Range(1, 100)]
+        public int? Age { get; set; }
+        [DataMember(Name = "Qualification")]
+        public string? Qualification { get; set; }
+        [DataMember(Name = "Address")]
+        public string? Address { get; set; }
+        [DataMember(Name = "Department")]
+        public string Department { get; set; }
+        public List<DepartmentClass> Departments { get; set; }
+
+    }
+}
